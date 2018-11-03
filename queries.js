de7cc2ce7148782b0a8b50fd984cffe9d7c504b8
Ang Jit Hwee <ang_jit_hwee@nyp.edu.sg>
2018-11-03 23:30:06 +0800

    Included Reviews

--- queries.js
+++ queries.js
@@ -1,10 +1,67 @@
+var usersData = [{"name":"Belson Heng","joinDate":"11 Sep 2017"},{"name":"Jit Hwee","joinDate":"21 Nov 2015"},{"name":"Ming Jun","joinDate":"08 Aug 2011"}];
+var platformsData = [{"name":"Carousell","url":"https://carousell.com/"},{"name":"Grab","url":"https://www.grab.com/sg/"},{"name":"Shopee","url":"https://shopee.sg/"}];
+var reviewsData =     [{
+      "Date" : "28 Oct 2018",
+      "Feedback" : "Great seller to deal with.",
+      "Rating" : "Positive",
+      "Written By" : "cookie monster",
+      "Platform" : "Carousell",
+      "User" : "Belson Heng"
+    },
+    {
+      "Date" : "18 Sep 2018",
+      "Feedback" : "Pleasant transaction!",
+      "Rating" : "Positive",
+      "Written By" : "bigbird",
+      "Platform" : "Carousell",
+      "User" : "Belson Heng"
+    },
+    {
+      "Date" : "10 Jun 2018",
+      "Feedback" : "Very friendly. Thank you :)",
+      "Rating" : "Positive",
+      "Written By" : "elmo",
+      "Platform" : "Carousell",
+      "User" : "Belson Heng"
+    },
+    {
+      "Date" : "28 Oct 2018",
+      "Feedback" : "Great seller to deal with.",
+      "Rating" : "Positive",
+      "Written By" : "haha5005",
+      "Platform" : "Grab",
+      "User" : "Belson Heng"
+    },
+    {
+      "Date" : "18 Sep 2018",
+      "Feedback" : "Very friendly. Thank you :)",
+      "Rating" : "Positive",
+      "Written By" : "nothingtosay",
+      "Platform" : "Grab",
+      "User" : "Belson Heng"
+    },
+    {
+      "Date" : "28 Oct 2018",
+      "Feedback" : "Great seller to deal with.",
+      "Rating" : "Positive",
+      "Written By" : "cookie monster",
+      "Platform" : "Shopee",
+      "User" : "Belson Heng"
+    },
+    {
+      "Date" : "18 Sep 2018",
+      "Feedback" : "Very friendly. Thank you :)",
+      "Rating" : "Positive",
+      "Written By" : "elmo",
+      "Platform" : "Shopee",
+      "User" : "Belson Heng"
+    }];
 
 function getAllPlatforms(req, res, next) {
-  var data = [{"name":"Carousell","url":"https://carousell.com/"},{"name":"Grab","url":"https://www.grab.com/sg/"},{"name":"Shopee","url":"https://shopee.sg/"}];
 
    res.status(200).json({
     status: 'success',
-    data: data,
+    data: platformsData,
     message: 'Retrieved ALL Online Platforms'
   });
 }
@@ -17,12 +74,34 @@ function getAllPlatforms(req, res, next) {
         });
 }
 
+function getSinglePlatform(req, res, next) {
+    var platform = req.params.platformName;
+    var retObj=null;
+
+    console.log("-->" + platform);
+
+    for(var i = 0 ; i< platformsData.length; i++){
+            var obj = platformsData[i];
+            var objName = obj["name"];
+
+            if (objName.toLowerCase().trim() == platform.toLowerCase().trim()){
+                retObj = obj;
+              }
+          }
+
+      res.status(200)
+        .json({
+          status: 'success',
+          data: retObj,
+          message: 'Retrieved platform if available'
+        });
+}
+
 function getAllUsers(req, res, next) {
-  var data = [{"name":"Belson Heng","joinDate":"11 Sep 2017"},{"name":"Jit Hwee","joinDate":"21 Nov 2015"},{"name":"Ming Jun","joinDate":"08 Aug 2011"}];
 
    res.status(200).json({
     status: 'success',
-    data: data,
+    data: usersData,
     message: 'Retrieved ALL Users'
   });
 }
@@ -36,39 +115,129 @@ function createUser(req, res, next) {
        });
 }
 
-// function getSinglePuppy(req, res, next) {
-//   var pupID = parseInt(req.params.id);
-//   db.one('select * from pups where id = $1', pupID)
-//     .then(function (data) {
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           data: data,
-//           message: 'Retrieved ONE puppy'
-//         });
-//     })
-//     .catch(function (err) {
-//       return next(err);
-//     });
-// }
-//
-// function createPuppy(req, res, next) {
-//   req.body.age = parseInt(req.body.age);
-//   db.none('insert into pups(name, breed, age, sex)' +
-//       'values(${name}, ${breed}, ${age}, ${sex})',
-//     req.body)
-//     .then(function () {
-//       res.status(200)
-//         .json({
-//           status: 'success',
-//           message: 'Inserted one puppy'
-//         });
-//     })
-//     .catch(function (err) {
-//       return next(err);
-//     });
-// }
-//
+function getSingleUser(req, res, next) {
+    var user = req.params.userName;
+    var retObj=null;
+
+    console.log("-->" + user);
+
+    for(var i = 0 ; i< usersData.length; i++){
+            var obj = usersData[i];
+            var objName = obj["User"];
+
+            if (objName.toLowerCase().trim() == user.toLowerCase().trim()){
+                retObj = obj;
+              }
+          }
+
+
+      res.status(200)
+        .json({
+          status: 'success',
+          data: retObj,
+          message: 'Retrieved user if available'
+        });
+}
+
+function getAllReviews(req, res, next) {
+
+   res.status(200).json({
+    status: 'success',
+    data: reviewsData,
+    message: 'Retrieved ALL reviews'
+  });
+}
+
+function getAllReviewsByPlatform(req, res, next) {
+
+  var platformName = req.params.platformName;
+  var retObj=[];
+
+  console.log("-->" + platformName);
+
+  for(var i = 0 ; i< reviewsData.length; i++){
+          var obj = reviewsData[i];
+          var objName = obj["Platform"];
+
+          if (objName.toLowerCase().trim() == platformName.toLowerCase().trim()){
+              retObj.push(obj)
+            }
+        }
+
+
+    res.status(200)
+      .json({
+        status: 'success',
+        data: retObj,
+        message: 'Retrieved Reviews by Platform Name'
+      });
+}
+
+function getAllReviewsByUser(req, res, next) {
+
+  var userName = req.params.userName;
+  var retObj=[];
+
+  console.log("-->" + userName);
+
+  for(var i = 0 ; i< reviewsData.length; i++){
+          var obj = reviewsData[i];
+          var objName = obj["User"];
+
+          if (objName.toLowerCase().trim() == userName.toLowerCase().trim()){
+              retObj.push(obj)
+            }
+        }
+
+
+    res.status(200)
+      .json({
+        status: 'success',
+        data: retObj,
+        message: 'Retrieved Reviews by specific user'
+      });
+}
+
+function getAllReviewsByPlatformByUser(req, res, next) {
+
+  var platformName = req.params.platformName;
+  var userName = req.params.userName;
+
+  var retObj=[];
+
+  console.log("-->" + platformName);
+  console.log("-->" + userName);
+
+  for(var i = 0 ; i< reviewsData.length; i++){
+          var obj = reviewsData[i];
+          var objName = obj["Platform"];
+
+          if (objName.toLowerCase().trim() == platformName.toLowerCase().trim()){
+              var user = obj["User"];
+              if(user.toLowerCase().trim() == userName.toLowerCase().trim())
+                retObj.push(obj)
+            }
+        }
+
+
+    res.status(200)
+      .json({
+        status: 'success',
+        data: retObj,
+        message: 'Retrieved Reviews by Platform Name & UserName'
+      });
+}
+
+function createReviewByPlatformByUser(req, res, next) {
+
+     res.status(200)
+       .json({
+         status: 'success',
+         message: 'Inserted one Review'
+       });
+}
+
+
 // function updatePuppy(req, res, next) {
 //   db.none('update pups set name=$1, breed=$2, age=$3, sex=$4 where id=$5',
 //     [req.body.name, req.body.breed, parseInt(req.body.age),
@@ -106,6 +275,13 @@ function createUser(req, res, next) {
 module.exports = {
   getAllPlatforms: getAllPlatforms,
   createPlatform: createPlatform,
+  getSinglePlatform:getSinglePlatform,
   getAllUsers: getAllUsers,
-  createUser:createUser
+  createUser:createUser,
+  getSingleUser:getSingleUser,
+  getAllReviews:getAllReviews,
+  getAllReviewsByPlatform:getAllReviewsByPlatform,
+  getAllReviewsByUser:getAllReviewsByUser,
+  getAllReviewsByPlatformByUser:getAllReviewsByPlatformByUser,
+  createReviewByPlatformByUser:createReviewByPlatformByUser
 };
--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,21 @@ var db = require('../queries');
  *       joinDate:
  *         type: string
  *
+ *   Review:
+ *     properties:
+ *       date:
+ *         type: string
+ *       feedback:
+ *         type: string
+ *       rating:
+ *         type: string
+ *       writtenBy:
+ *         type: string
+ *       platformName:
+ *         type: string
+ *       userName:
+ *         type: string
+ *
  */
 
 /**
@@ -61,6 +76,28 @@ router.get('/api/platforms', db.getAllPlatforms);
  */
 router.post('/api/platform', db.createPlatform);
 
+/**
+ * @swagger
+ * /api/platforms/{platformName}:
+ *   get:
+ *     tags:
+ *       - Platform
+ *     description: Returns a single Platform Data
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: platformName
+ *         description: platform's name
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: A single platform
+ *         schema:
+ *           $ref: '#/definitions/Platform'
+ */
+router.get('/api/platforms/:platformName', db.getSinglePlatform);
 
 /**
  * @swagger
@@ -99,8 +136,149 @@ router.get('/api/users', db.getAllUsers);
  *       200:
  *         description: Successfully created
  */
-router.get('/api/users', db.createUser);
+router.post('/api/users', db.createUser);
 
 
+/**
+ * @swagger
+ * /api/users/{userName}:
+ *   get:
+ *     tags:
+ *       - User
+ *     description: Returns a single User Data
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: userName
+ *         description: user's name
+ *         in: path
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: A single user
+ *         schema:
+ *           $ref: '#/definitions/User'
+ */
+router.get('/api/users/:userName', db.getSingleUser);
+
+/**
+ * @swagger
+ * /api/reviews:
+ *   get:
+ *     tags:
+ *       - Review
+ *     description: Return all Reviews
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of reviews
+ *         schema:
+ *           $ref: '#/definitions/Review'
+ */
+router.get('/api/reviews', db.getAllReviews);
+
+/**
+ * @swagger
+ * /api/reviews/platform/{platformName}:
+ *   get:
+ *     tags:
+ *       - Review
+ *     description: Return all Reviews by specific platform
+ *     produces:
+ *       - application/json
+ *     parameters:
+*       - name: platformName
+*         description: platform's name
+*         in: path
+*         required: true
+*         type: string
+ *     responses:
+ *       200:
+ *         description: An array of reviews by specific platform
+ *         schema:
+ *           $ref: '#/definitions/Review'
+ */
+router.get('/api/reviews/platform/:platformName', db.getAllReviewsByPlatform);
+
+/**
+ * @swagger
+ * /api/reviews/user/{userName}:
+ *   get:
+ *     tags:
+ *       - Review
+ *     description: Return all Reviews by specific user
+ *     produces:
+ *       - application/json
+ *     parameters:
+*       - name: userName
+*         description: user's name
+*         in: path
+*         required: true
+*         type: string
+ *     responses:
+ *       200:
+ *         description: An array of reviews by specific user
+ *         schema:
+ *           $ref: '#/definitions/Review'
+ */
+router.get('/api/reviews/user/:userName', db.getAllReviewsByUser);
+
+/**
+ * @swagger
+ * /api/reviews/platform/{platformName}/user/{userName}:
+ *   get:
+ *     tags:
+ *       - Review
+ *     description: Return all Reviews by specific platform and user
+ *     produces:
+ *       - application/json
+ *     parameters:
+*       - name: platformName
+*         description: platform's name
+*         in: path
+*         required: true
+*         type: string
+*       - name: userName
+*         description: user's name
+*         in: path
+*         required: true
+*         type: string
+ *     responses:
+ *       200:
+ *         description: An array of reviews by specific platform
+ *         schema:
+ *           $ref: '#/definitions/Review'
+ */
+router.get('/api/reviews/platform/:platformName/user/:userName', db.getAllReviewsByPlatformByUser);
+
+/**
+ * @swagger
+ * /api/reviews/platform/{platformName}/user/{userName}:
+ *   post:
+ *     tags:
+ *       - Review
+ *     description: Create a new Review for a specific User & specific platform
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: userName
+ *         description: User object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/User'
+ *       - name: platformName
+ *         description: Platform object
+ *         in: body
+ *         required: true
+ *         schema:
+ *           $ref: '#/definitions/Platform'
+ *     responses:
+ *       200:
+ *         description: Successfully created a Review
+ */
+router.post('/api/reviews/platform/:platformName/user/:userName', db.createReviewByPlatformByUser);
 
 module.exports = router;
